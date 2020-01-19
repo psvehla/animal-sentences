c14d5f4e21adddf8939548d9338da3e40565f0ea
peter <psvehla@hotmail.com>
2020-01-20 00:54:59 +1100

    Tidy. Layout tidy.

--- src/App.test.js
+++ src/App.test.js
@@ -5,43 +5,54 @@ import NumericInput from 'react-numeric-input';
 import App from './App';
 
 it('renders correctly', () => {
+
   const tree = renderer
     .create(<App page="https://localhost:3000">AnimalSentences</App>)
     .toJSON();
+
   expect(tree).toMatchSnapshot();
 });
 
 test('test prefixLen input', () => {
+
   const TEST_VALUE = 10
   const changePrefixLenMock = jest.fn();
+
   const event = {
     preventDefault() {},
     target: { value: TEST_VALUE}
   };
+
   const component = shallow(<NumericInput onChange={changePrefixLenMock} />);
   component.find('input').simulate('change', event);
   expect(changePrefixLenMock).toBeCalled();
 });
 
 test('test suffixLen input', () => {
+
   const TEST_VALUE = 10
   const changeSuffixLenMock = jest.fn();
+
   const event = {
     preventDefault() {},
     target: { value: TEST_VALUE}
   };
+
   const component = shallow(<NumericInput onChange={changeSuffixLenMock} />);
   component.find('input').simulate('change', event);
   expect(changeSuffixLenMock).toBeCalled();
 });
 
 test('test numberOfSentences input', () => {
+
   const TEST_VALUE = 10
   const changeNumberOfSentencesMock = jest.fn();
+
   const event = {
     preventDefault() {},
     target: { value: TEST_VALUE}
   };
+
   const component = shallow(<NumericInput onChange={changeNumberOfSentencesMock} />);
   component.find('input').simulate('change', event);
   expect(changeNumberOfSentencesMock).toBeCalled();
